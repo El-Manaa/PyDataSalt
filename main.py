5a897586f5692bd374b7d0122833486889ebd3f9
El-Manaa <menaamouhamed88@gmail.com>
2025-08-24 19:38:59 +0100

    commit 1

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from dbc import connect, write_data
 async def main():
     with create_firefox_driver() as driver:
         atoms = scrape(driver)
-        async for atom in atoms:
-            print(atom)
-        # coll = connect()
-        # await write_data(atoms, coll)
+        coll = connect()
+        await write_data(atoms, coll)
 
 
 if __name__ == "__main__":
